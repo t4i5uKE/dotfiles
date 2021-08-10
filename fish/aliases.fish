28cb8b645deea5ac7189e0b32d2cde031cb29c60
morero <taisuke.1202morero@gmail.com>
2021-08-10 15:41:09 +0900

    💪 Update config.fish, aliases.fish

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,8 +1,23 @@
 # lsd
-alias ls 'lsd'
+alias ls lsd
 alias l 'ls -l'
 alias la 'ls -a'
 alias lla 'ls -la'
 alias lt 'ls --tree'
 
 # quick edits
+alias ev 'vim ~/.vimrc'
+alias etmux 'vim ~/.tmux.conf'
+alias efa 'vim ~/.config/fish/aliases.fish'
+alias efc 'vim ~/.config/fish/config.fish'
+alias sfc 'source ~/.config/fish/config.fish'
+
+# clear
+alias cls clear
+
+# cli
+alias k kubectl
+alias h helm
+alias i istioctl
+alias d docker
+alias gc gcloud
--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,25 @@
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # nodenv
+    source (nodenv init -|psub)
 end
+
+# asdf
+source /usr/local/opt/asdf/asdf.fish
+
+# golang
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+
+# google cloud sdk
+# The next line updates PATH for the Google Cloud SDK.
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc'
+bass source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
+
+
+# command history (ctrl + Y)
+bind \cy __fzf_reverse_isearch_with_copy
+
+# $fish_user_paths
+set -x PATH /usr/local/bin $PATH
