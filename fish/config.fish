d7768391dbac5dedf7b7b69c31d4de11c0663936
morero <taisuke.1202morero@gmail.com>
2021-08-10 17:33:22 +0900

    🚿 Remove command gcloud completions

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 . ~/.config/fish/aliases.fish
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     # nodenv
     source (nodenv init -|psub)
 end
@@ -12,12 +11,6 @@ source /usr/local/opt/asdf/asdf.fish
 # golang
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
-
-# google cloud sdk
-# The next line updates PATH for the Google Cloud SDK.
-source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc'
-bass source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
-
 
 # command history (ctrl + Y)
 bind \cy __fzf_reverse_isearch_with_copy
