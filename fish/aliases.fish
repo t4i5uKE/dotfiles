02df2a0985650e9f5149d60563fc6702c6cb36a9
morero <taisuke.1202morero@gmail.com>
2021-10-03 05:40:18 +0900

    ✨ Add fish

---
+++ fish/aliases.fish
@@ -0,0 +1,28 @@
+# lsd
+alias ls lsd
+alias l 'ls -l'
+alias la 'ls -a'
+alias lla 'ls -la'
+alias lt 'ls --tree'
+
+# quick edits
+alias ev 'vim ~/.vimrc'
+alias etmux 'vim ~/.tmux.conf'
+alias efa 'vim ~/.config/fish/aliases.fish'
+alias efc 'vim ~/.config/fish/config.fish'
+alias sfc 'source ~/.config/fish/config.fish'
+
+# clear
+alias cls clear
+
+# cli
+alias k kubectl
+alias kk 'kubectl krew'
+alias h helm
+alias i istioctl
+alias d docker
+alias gc gcloud
+
+# docker
+alias dclean 'docker rm -v (docker ps -a -q -f status=exited)'
+alias dclean-none 'docker rmi (docker images -f "dangling=true" -q)'
